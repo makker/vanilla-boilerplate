26ba6cf6870e4f1e6d6d5d2298186ddb93478e5d
Markus Ruottu <makkeruottu@gmail.com>
2018-02-25 17:32:10 +0200

    add polyfill for NodeList.forEach

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+/* Polyfill for NodeList.forEach (all IE) from 
+ * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
+ */
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
