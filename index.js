f6fc0c8a895c229ba10ddf60bbc11fa8b4d70fc7
Markus Ruottu <makkeruottu@gmail.com>
2018-03-03 12:44:14 +0200

    added polyfills for IE

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
-
-/* Polyfill for NodeList.forEach (all IE) from 
+/* Polyfill for NodeList.forEach from 
  * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
  */
 if (window.NodeList && !NodeList.prototype.forEach) {
@@ -11,3 +10,81 @@ if (window.NodeList && !NodeList.prototype.forEach) {
         }
     };
 }
+
+/* Polyfill for Object.assign from (for IE)
+ * https://gist.github.com/spiralx/68cf40d7010d829340cb
+ */
+if (!Object.assign) {
+    Object.defineProperty(Object, 'assign', {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (target) {
+            'use strict';
+            if (target === undefined || target === null) {
+                throw new TypeError('Cannot convert first argument to object');
+            }
+
+            var to = Object(target);
+            for (var i = 1; i < arguments.length; i++) {
+                var nextSource = arguments[i];
+                if (nextSource === undefined || nextSource === null) {
+                    continue;
+                }
+                nextSource = Object(nextSource);
+
+                var keysArray = Object.keys(Object(nextSource));
+                for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+                    var nextKey = keysArray[nextIndex];
+                    var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+                    if (desc !== undefined && desc.enumerable) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+            return to;
+        }
+    });
+}
+
+/* Polyfill for ChildNode.remove from (for IE)
+ * https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+ */
+(function (arr) {
+    arr.forEach(function (item) {
+        if (item.hasOwnProperty('remove')) {
+            return;
+        }
+        Object.defineProperty(item, 'remove', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function remove() {
+                if (this.parentNode !== null)
+                    this.parentNode.removeChild(this);
+            }
+        });
+    });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
+// Define global event added function
+var addEvent = function (object, type, callback) {
+    if (object == null || typeof (object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on" + type] = callback;
+    }
+};
+
+function initCommon() {
+    // If dev url param then add class dev to body to enable some dev enhancements
+    // This feature should be removed before deploying to production
+    if (window.location.href.indexOf("?dev") > -1) {
+        document.body.classList.add("dev");
+    }
+};
+
+addEvent(document, "DOMContentLoaded", initCommon);
